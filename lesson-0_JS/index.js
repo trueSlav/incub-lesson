3313e338ff8e2ca61b0e9d894dd5f4c1a4e0bc46
trueSlav <2001idercach@gmail.com>
2023-12-04 20:10:46 +0300

    A first js file/ A index.js file

---
+++ lesson-0_JS/index.js
@@ -0,0 +1,14 @@
+let name = 'it-incubator';
+
+let graduatesCount = 2000;
+
+let areYouChampion = false;
+
+document.write('<h1>', name, '</h1>');
+
+document.write('<input type="number" value=" ' + graduatesCount + ' ">');
+
+document.write('<input type="checkbox"', areYouChampion ? 'checked' : '', '>');
+
+
+
