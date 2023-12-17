c9e2f48c9be09caaec3d3d06a67aa720167c4bc9
trueSlav <2001idercach@gmail.com>
2023-12-17 18:43:47 +0300

    a2

--- pre-course-hw-js/lesson-01/02.js
+++ pre-course-hw-js/lesson-01/02.js
@@ -1,5 +1,4 @@
 // code
-alert('ssssssssssssssssssssssssssssssssssyka')
 let number = Math.floor(Math.random() * 100);
 
 (number % 2) > 0 ? console.log(`${number}:Это нечетное число`) : console.log(`${number}:Это четное число`);
