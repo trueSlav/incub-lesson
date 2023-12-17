db87dbab740b7582435fe337d8341217d6fd6a04
trueSlav <2001idercach@gmail.com>
2023-12-17 18:53:52 +0300

    a4

--- pre-course-hw-js/lesson-01/02.js
+++ pre-course-hw-js/lesson-01/02.js
@@ -1,4 +1,4 @@
 // code
-let number = Math.floor(Math.random() * 100)
+let number = Math.floor(Math.random() * 100);
 
-(number % 2) > 0 ? console.log(`${number}:Это нечетное число`) : console.log(`${number}:Это четное число`)
+(number % 2) > 0 ? console.log(`${number}:Это нечетное число`) : console.log(`${number}:Это четное число`);
